cbbcbc117e24fd7e268028a983113a9d860ba3b3
CRomao <ciceroromao1923@gmail.com>
2019-08-06 16:32:45 -0300

    deletado outros arquivos

--- impressao.h
+++
@@ -1,1 +0,0 @@
-void imprimir(TPPilhaCarta *pilhasCarta); 
--- structsPacienciaLan.h
+++
@@ -1,4 +0,0 @@
-typedef struct carta TPCarta;
-typedef struct  pilhaCarta TPPilhaCarta;
-typedef struct jogador TPJogador;
-typedef struct historico TPHistorico; 
